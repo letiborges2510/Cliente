74e3ce8f3df2d02562b4515a9e76e06c2af0f982
Letícia Borges <99838964+letiborges2510@users.noreply.github.com>
2026-04-26 03:26:06 -0300

    Delete main directory

--- spring/src/main/java/com/example/cliente/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cliente.repository;
-
-import com.example.cliente.model.Cliente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClienteRepository extends MongoRepository<Cliente, String> {
-}
